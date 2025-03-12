17bac10169b4e5b8201095bd840d83d899736c31
Luigi Ajello <luigipedrosoajello@gmail.com>
2025-03-11 21:25:36 -0300

    .

---
+++ src/aula4/MinhaClasse.java
@@ -0,0 +1,31 @@
+package aula4;
+
+public class MinhaClasse {
+	
+	private int alpha; //acesso privado
+	
+	public int beta;  //acesso público
+	
+	int gamma; //acesso padrão 
+
+	 void acessar_alpha (int alpha) {
+	   
+		
+		
+		return alpha;
+
+		
+		       
+	 	}
+	
+	 int get_alpha() {
+		   
+			
+			return alpha;
+			
+			
+			       
+		 	}
+}
+
+
